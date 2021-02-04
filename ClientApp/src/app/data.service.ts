77dd61db0d896afc1f7ff372aa714f0045f01d7a
bobis1888 <bobis1888@gmail.com>
2021-02-04 23:50:51 +0500

    add first page and postgres connection

--- ClientApp/src/app/data.service.ts
+++ ClientApp/src/app/data.service.ts
@@ -1,34 +1,31 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Product} from './product';
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Product } from './product';
 
 @Injectable()
 export class DataService {
 
-  private url = "/api/products";
+    private url = "/api/products";
 
-  constructor(private http: HttpClient) {
-    
-  }
+    constructor(private http: HttpClient) {
+    }
 
-  getProducts() {
-    return this.http.get(this.url);
-  }
+    getProducts() {
+        return this.http.get(this.url);
+    }
 
-  getProduct(id: number) {
-    return this.http.get(this.url + '/' + id);
-  }
+    getProduct(id: number) {
+        return this.http.get(this.url + '/' + id);
+    }
 
-  createProduct(product: Product) {
-    return this.http.post(this.url, product);
-  }
+    createProduct(product: Product) {
+        return this.http.post(this.url, product);
+    }
+    updateProduct(product: Product) {
 
-  updateProduct(product: Product) {
-    return this.http.put(this.url,product);
-  }
-
-  deleteProduct(id: number) {
-    return this.http.delete(this.url + '/' + id);
-  }
-
+        return this.http.put(this.url, product);
+    }
+    deleteProduct(id: number) {
+        return this.http.delete(this.url + '/' + id);
+    }
 }
