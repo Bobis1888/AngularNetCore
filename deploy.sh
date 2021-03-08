e6cecfcee680b86d0a424dfc501b680b406dfc16
bobis1888 <bobis1888@gmail.com>
2021-03-09 00:29:41 +0500

    ref

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 dotnet publish -c Release
 cp Dockerfile ./bin/Release/netcoreapp3.1/publish/Dockerfile
 docker build -t netreader ./bin/Release/netcoreapp3.1/publish
-heroku container:push web -a netreader
-heroku container:release web -a netreader
+heroku container:push web -a netreader2
+heroku container:release web -a netreader2
